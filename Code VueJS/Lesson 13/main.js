985d7de1d14dc41afb6f0655b9dc5b34d6388d29
Vu Cong Minh <vukongminh@gmail.com>
2022-08-03 17:23:30 +0700

    Push code VueJS

---
+++ Code VueJS/Lesson 13/main.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    listBlogs: [
+      {
+        "id": 1,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+      {
+        "id": 2,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+      {
+        "id": 3,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+      {
+        "id": 4,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+      {
+        "id": 5,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+      {
+        "id": 6,
+        "userId": 1,
+        "title": "Dinoland",
+        "body": "Vũ trụ game metaverse đang là một trong những chủ đề nóng nhất hiện tại bởi không gian game đa dạng và khả năng kiếm tiền hấp dẫn. Sức hút này không chỉ với những người đầu tư lâu năm mà còn lan tỏa đến các newbie mới tìm hiểu về thị trường. Trong bài viết gần đây CoinExpress đã đem đến bạn thông tin về dự án game Nitro League và hôm nay Dinoland chính là cái tên tiếp theo được nhắc đến."
+      },
+    ],
+    scores: {
+      math: 8.0,
+      english: 6.0,
+      physics: 7.0
+    }
+  },
+  methods: {
+
+  }
+})
