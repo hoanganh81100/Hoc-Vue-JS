9d913abe82b8794a2d1f30022ce83deebb060464
Vu Cong Minh <vukongminh@gmail.com>
2022-08-05 18:31:43 +0700

    Push code VueJS

---
+++ Vue-CLI/Lesson21-27/src/components/CompFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer><h1>{{ footer }}</h1></footer>
+</template>
+
+<script>
+export default {
+  name: 'comp-footer',
+  props: {
+    footer: {
+      type: String,
+      default: 'Footer'
+    }
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue-CLI/Lesson21-27/src/components/CompHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header><h1>{{ header }}</h1></header>
+</template>
+
+<script>
+export default {
+  name: 'comp-hearder',
+  props: {
+    header: String
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
